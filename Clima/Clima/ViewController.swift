fce9220b7fea1abf92b29606280c607abbeb047c
Claudia Maganhi <claudia.maganhi@gmail.com>
2020-05-07 11:15:04 -0300

    Create view

--- Clima/Clima/ViewController.swift
+++ Clima/Clima/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func search(_ sender: UIButton) {
+    }
+    
+    @IBAction func updateLocation(_ sender: UIButton) {
+    }
+    
+    
 }
 
